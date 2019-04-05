07d5992f2292600b351160ac0a688ffd4c3d5a16
NQuintana <desarrollador13@wog.co>
2019-04-05 08:50:26 -0500

    Actualizacion de modelo de base de datos
    se actualizan los crud de las tablas para la base de datos se actualiza el modelado y las propiedades del servidor embebido de spring boot, adicional se generan algunos insert de pruebas en archivo Import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,14 +1,27 @@
-/*Insert activos fijos */
+﻿/* Insercion activos fijos */
 
 INSERT INTO activos_fijos (id, creado, creado_por, actualizado, actualizado_por, nombre, descripcion, estado) VALUES(1, '2018-01-01 11:33:52.12315', 101, '2018-01-01 11:33:52.12315', 101, 'bienes inmuebles', null, 'activo');
 INSERT INTO activos_fijos (id, creado, creado_por, actualizado, actualizado_por, nombre, descripcion, estado) VALUES(2, '2018-01-01 11:33:52.12315', 101, '2018-01-01 11:33:52.12315', 101, 'maquinaria', null, 'activo');
 INSERT INTO activos_fijos (id, creado, creado_por, actualizado, actualizado_por, nombre, descripcion, estado) VALUES(3, '2018-01-01 11:33:52.12315', 101, '2018-01-01 11:33:52.12315', 101, 'material de oficina', null, 'activo');
 
-/*Insert area*/
+/* Insercion area*/
 INSERT INTO areas (id, creado, creado_por, actualizado, actualizado_por, nombre, estado) VALUES(1, '2018-01-01 11:33:52.12315', 101, '2018-01-01 11:33:52.12315', 101, 'Explotacion minera', 'activo');
 
-/*Insert persona*/
+/* Insercion persona*/
 INSERT INTO personas (id, creado, creado_por, actualizado, actualizado_por, nombre, cargo, identificacion, telefono, estado, email) VALUES (1, '2018-01-01 11:33:52.12315', 101, '2018-01-01 11:33:52.12315', 101, 'Carlos joaquin peña quintero', 'conductor maquinaria pesada', '1024542755', '3174944581', 'activo', 'brayannicolas1993@hotmail.com');
 
-/*Insert tipos de activos fijos*/
+/*Insercion tipos de activos fijos*/
 INSERT INTO tipos_activos (id, creado, creado_por, actualizado, actualizado_por, nombre, descripcion, serial, numero_inventario, peso, alto, ancho, largo , valor_compra, fecha_compra, fecha_baja, estado, color, activo_fijo_id, area_id, persona_id) VALUES (1, '2018-01-01 11:33:52.12315', 101, '2018-01-01 11:33:52.12315', 101, 'Retroexcavadora', 'Trabajo de excavación', '112358569', '001', 150.00, 2.00, 180.00, 180.00, 150000000.00, '2018-01-01', null, 'activo', 'amarillo', 2, 1, 1);
+
+/*Insercion autPersonas*/
+INSERT INTO aut_personas (id, creado, creado_por, actualizado, actualizado_por, usuario, clave, estado) VALUES (1, '2018-01-01 11:33:52.12315', 101, '2018-01-01 11:33:52.12315', 101, "admin", '$2a$10$C3Uln5uqnzx/GswADURJGOIdBqYrly9731fnwKDaUdBkt/M3qvtLq', 'activo');
+INSERT INTO aut_personas (id, creado, creado_por, actualizado, actualizado_por, usuario, clave, estado) VALUES (2, '2018-01-01 11:33:52.12315', 101, '2018-01-01 11:33:52.12315', 101, "nicolas", '$2a$10$RmdEsvEfhI7Rcm9f/uZXPebZVCcPC7ZXZwV51efAvMAp1rIaRAfPK', 'activo');
+
+/*Insercion roles*/
+INSERT INTO roles (id, creado, creado_por, actualizado, actualizado_por, nombre, descripcion, estado) VALUES (1, '2018-01-01 11:33:52.12315', 101, '2018-01-01 11:33:52.12315', 101, 'ROLE_ADMIN', null, 'activo');
+INSERT INTO roles (id, creado, creado_por, actualizado, actualizado_por, nombre, descripcion, estado) VALUES (2, '2018-01-01 11:33:52.12315', 101, '2018-01-01 11:33:52.12315', 101, 'ROLE_USER', null, 'activo');
+
+/*Insercion aut_personas_roles*/
+INSERT INTO aut_personas_roles (aut_personas_id, rol_id) VALUES (1, 1);
+INSERT INTO aut_personas_roles (aut_personas_id, rol_id) VALUES (2, 2);
+INSERT INTO aut_personas_roles (aut_personas_id, rol_id) VALUES (2, 1);
